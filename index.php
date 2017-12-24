0c0d046914f5239ba25faddd7f1d8608686b41ba
Chijioke Okwunakwe <15726558+omazim@users.noreply.github.com>
2017-12-24 15:44:18 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+header("Cache Control: must-revalidate, no-store, max-age=60");
+include_once "/html/frags/header.html";
+include_once "/html/frags/index.html";
+include_once "/html/frags/footer.html";
